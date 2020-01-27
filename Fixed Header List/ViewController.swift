66e27d6cc2a33cb2b45be78d834055977c0e82ec
Rodrigo Giglio <rodrigo.giglio@poatek.com>
2020-01-27 13:26:47 -0300

    starts repository with empty project

---
+++ Fixed Header List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fixed Header List
+//
+//  Created by Rodrigo on 27/01/2020.
+//  Copyright © 2020 Rodrigo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Fixed Header ListTests/Fixed_Header_ListTests.swift
@@ -0,0 +1,34 @@
+//
+//  Fixed_Header_ListTests.swift
+//  Fixed Header ListTests
+//
+//  Created by Rodrigo on 27/01/2020.
+//  Copyright © 2020 Rodrigo. All rights reserved.
+//
+
+import XCTest
+@testable import Fixed_Header_List
+
+class Fixed_Header_ListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
